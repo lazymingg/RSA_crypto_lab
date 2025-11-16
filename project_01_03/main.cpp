7df05627c1975193d2ec07b03f8a595da2180b0a
lazyming <lazyming@vnpt.vn>
2025-11-16 13:56:21 +0700

    submission branch

--- project_01_03/main.cpp
+++ project_01_03/main.cpp
@@ -29,66 +29,25 @@ BigInt RSA_encrypt(const BigInt &message, const BigInt &e, const BigInt &n)
 {
     return mod_exp(message, e, n);
 }
+
 BigInt RSA_decrypt(const BigInt &ciphertext, const BigInt &d, const BigInt &n)
 {
     return mod_exp(ciphertext, d, n);
 }
 
-// void testing_mode()
-// {
-//     string dir = "./project_01_03/";
-//     int test_cases = 19;
-//     for (int i = 0; i < test_cases; i++)
-//     {
-//         ifstream ifile(dir + "test_" + (i < 10 ? "0" : "") + to_string(i) + ".inp");
-//         string modulus_str;
-//         string exponent_str;
-//         string message_str;
-
-//         getline(ifile, modulus_str);
-//         getline(ifile, exponent_str);
-//         getline(ifile, message_str);
-
-//         reverse(modulus_str.begin(), modulus_str.end());
-//         reverse(exponent_str.begin(), exponent_str.end());
-//         reverse(message_str.begin(), message_str.end());
-
-//         BigInt modulus(modulus_str);
-//         BigInt exponent(exponent_str);
-//         BigInt message(message_str);
-//         BigInt ciphertext = RSA_encrypt(message, exponent, modulus);
-
-//         string output = ciphertext.to_string();
-//         reverse(output.begin(), output.end());
-//         // convert to uppercase
-//         transform(output.begin(), output.end(), output.begin(), ::toupper);
-
-//         ifstream ofile(dir + "test_0" + to_string(i) + ".out");
-//         string expected_output;
-//         getline(ofile, expected_output);
-        
-//         cout << "Test case " << i << ": ";
-//         cout << "Modulus: " << modulus_str << ", Exponent: " << exponent_str << ", Message: " << message_str << endl;
-//         cout << (output == expected_output ? "Passed" : "Failed") << endl;
-//         cout << "Expected: " << expected_output << endl;
-//         cout << "Got     : " << output << endl;
-//         cout << endl;
-//         ifile.close();
-//         ofile.close();
-//     }
-// }
-
 int main(int argc, char* argv[])
 {
     // testing_mode();
     // return 0;
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <input_file>\n";
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <input_file> <output_file>\n";
         return 1;
     }
 
-    // cout << "Program name: " << argv[0] << endl;
-    // cout << "Input file: " << argv[1] << endl;
+    cout << "Program name: " << argv[0] << endl;
+    cout << "Input file: " << argv[1] << endl;
+    cout << "Output file :" << argv[2] << endl;
+
     ifstream ifile(argv[1]);
 
     if (!ifile.is_open()) {
@@ -117,13 +76,17 @@ int main(int argc, char* argv[])
     BigInt ciphertext = RSA_encrypt(message, exponent, modulus);
     string output = ciphertext.to_string();
     reverse(output.begin(), output.end());
-    // cout << "Modulus: " << modulus << endl;
-    // cout << "Exponent: " << exponent << endl;
-    // cout << "Original message: " << message << endl;
-    // string output = ciphertext.to_string();
-    // reverse(output.begin(), output.end());
-    // cout << "Ciphertext or Decrypt Message: " << output << endl;
-    cout << output << endl;
+
+
+    ofstream ofile(argv[2]);
+    if (!ofile.is_open()) {
+        cerr << "Error opening file: " << argv[2] << endl;
+        return 1;
+    }
+    // upper case
+    transform(output.begin(), output.end(), output.begin(), ::toupper);
+    ofile << output;
+    ofile.close();
 
     return 0;
 }
--- test3.py
+++ test3.py
@@ -1,13 +1,13 @@
 from pwn import *
-import re
 
 context.binary = './project_01_03/main'
 dir = './project_01_03/'
 
 ntest = 10
 for i in range(ntest):
-    p = process([dir + 'main', dir + f'test_{i:02d}.inp'], display=False)
+    p = process([dir + 'main', dir + f'test_{i:02d}.inp'])
     stdout = p.recvall(timeout=30).decode(errors='ignore')[:-1]
+    print("")
 
     got = stdout.upper()
     print('Got:', got)
@@ -18,3 +18,4 @@ for i in range(ntest):
     
     print(f'Test case {i}: ', end='')
     print('PASS' if got == expected else 'FAIL')
+    print('--------------------')
